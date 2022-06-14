caeca3c9f0ff770156d5a61721c09bee35c38d12
Franco <francoguardini10@gmail.com>
2022-06-14 22:37:40 +0200

    cvs added

--- components/CvSelect.tsx
+++ components/CvSelect.tsx
@@ -2,17 +2,17 @@ export const CvSelect = () => {
   return (
     <div className="d-flex justify-content-center fixed-top mt-5">
       <div className="cv-select mt-1 shadow">
-        <a className="" download href="/vercel.svg">
+        <a className="" download href="/My-euroCv-it.pdf">
           <div className="cv--select-option p-2">
             <h5>Euro IT</h5>
           </div>
         </a>
-        <a className="" download href="/vercel.svg">
+        <a className="" download href="/My-euroCv-en.pdf">
           <div className="cv--select-option p-2">
             <h5>Euro EN</h5>
           </div>
         </a>
-        <a className="" download href="/vercel.svg">
+        <a className="" download href="/my-cv.pdf">
           <div className="cv--select-option p-2">
             <h5>International</h5>
           </div>
--- components/about/Description.tsx
+++ components/about/Description.tsx
@@ -78,17 +78,18 @@ export const Description = () => {
                     <h2>Who i am?</h2>
                   </div>
                   <p>
-                    In few words, i&apos;m a man who loves to develop high quality
-                    web apps. I&apos;m specializing in JavaScript/TypeScript techs,
-                    like Reactjs, Nextjs or Nodejs. I also developed projects
-                    with Python. I was born in Argentina but i&apos;m italian
-                    cittizen and i live in Italy now. I love to learn new
-                    things, and if there are about software world, better!
+                    In few words, i&apos;m a man who loves to develop high
+                    quality web apps. I&apos;m specializing in
+                    JavaScript/TypeScript techs, like Reactjs, Nextjs or Nodejs.
+                    I also developed projects with Python. I was born in
+                    Argentina but i&apos;m italian cittizen and i live in Italy
+                    now. I love to learn new things, and if there are about
+                    software world, better!
                     <br /> <br />
-                    For me, the continue learning is very important. That&apos;s why
-                    i&apos;m on the way to become an {" "}
+                    For me, the continue learning is very important. That&apos;s
+                    why i&apos;m on the way to become an{" "}
                     <span className="fw-bold">
-                      Informatic/Electronic Engineer
+                      Informatic/Electronic Engineer.
                     </span>
                   </p>
                 </div>
@@ -97,7 +98,7 @@ export const Description = () => {
                 <div className="">
                   <div className="mb-5 mt-3">
                     <h2 className="">Chi sono io?</h2>
-                  </div>   
+                  </div>
                   <p>
                     In poche parole, sono un uomo che ama sviluppare &quot;high
                     quality&quot; web apps. Mi sto specializzando in
@@ -107,10 +108,10 @@ export const Description = () => {
                     Italia. Mi piace imparare cose nuove, e se sono di software,
                     meglio!
                     <br /> <br />
-                    Per me il l&apos;apprendimento continuo è molto importante. Ecco
-                    perchè sono in il percorso di diventare
+                    Per me il l&apos;apprendimento continuo è molto importante.
+                    Ecco perchè sono in il percorso di diventare un{" "}
                     <span className="fw-bold">
-                      Ingegniero Informatico/Elettronico
+                      Ingegniero Informatico/Elettronico.
                     </span>
                   </p>
                 </div>
@@ -129,9 +130,9 @@ export const Description = () => {
                     en Italia. Me gusta aprender cosas nuevas, y si son sobre el
                     mundo del software, mejor! <br /> <br />
                     Para mí el aprendizaje continuo es muy importante. Es por
-                    eso que estoy en camino a convertirme en
+                    eso que estoy en camino a convertirme en {" "}
                     <span className="fw-bold">
-                      Ingeniero Informatico/Electrónico
+                      Ingeniero Informatico/Electrónico.
                     </span>
                   </p>
                 </div>
